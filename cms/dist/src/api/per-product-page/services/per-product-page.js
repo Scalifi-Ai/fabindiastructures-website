b2d0b4951012a1075eba3afa26c0c319b1fffd8f
RonyAibloq-1503 <rohan.nikam@aibloq.com>
2024-12-18 20:57:09 +0530

    added cms for website

---
+++ cms/dist/src/api/per-product-page/services/per-product-page.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * per-product-page service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::per-product-page.per-product-page');
---
+++ cms/src/api/per-product-page/controllers/per-product-page.ts
@@ -0,0 +1,7 @@
+/**
+ * per-product-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::per-product-page.per-product-page');
